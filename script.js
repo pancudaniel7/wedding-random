d46152e3b4f1e98016e80eebcb37443a40737d70
Daniel <pancudaniel7@users.noreply.github.com>
2023-08-04 00:19:52 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// ocument.getElementById('big-number').innerText = 456; // Example: changing the number
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 101) + 1; // Generates a random number between 1 and 101
+}
+
+function updateNumber() {
+    const bigNumberElement = document.getElementById('big-number');
+    bigNumberElement.classList.add('fade-out');
+    
+    setTimeout(() => {
+        bigNumberElement.innerText = generateRandomNumber();
+        bigNumberElement.classList.remove('fade-out');
+    }, 100);
+}
+
+let interval;
+
+clearInterval(interval);
+
+interval = setInterval(updateNumber, 200);
+setTimeout(() => {
+    clearInterval(interval);
+}, 3000);
